5fd5dee38d5c7e316933198d7041b73218cc04ec
erick hilda andreas <erickh.andreas@gmail.com>
2019-06-16 12:56:17 +0700

    feat: adding event-page layout

--- pages/events.vue
+++ pages/events.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>events</div>
+  <v-content class="pa-0">
+    <v-container fluid class="pt-3 mb-0 grey lighten-5">
+      <v-layout wrap align-center justify-center row fill-height class="my-0">
+        <v-flex xs12 md10 class="my-0">
+          <p class="google-font mb-0" style="font-size:200%;color:#0277bd">
+            {{ ChapterDetails.ChapterName }}'s Events
+          </p>
+          <p class="google-font mt-0 mb-0" style="font-size:110%">
+            Questions? Please contact {{ ChapterDetails.ChapterEmail }}.
+          </p>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container fluid class="grey lighten-5">
+      <v-layout wrap align-center justify-center row fill-height>
+        <v-flex xs12 md10 class="">
+          <app-event-upcoming />
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container fluid class="">
+      <v-layout wrap align-center justify-center row fill-height>
+        <v-flex xs12 md10 class="">
+          <app-event-past />
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
-export default {}
+import ChapterDetails from '~/assets/data/chapterDetails.json'
+import AppEventUpcoming from '~/components/event/AppEventUpcoming'
+import AppEventPast from '~/components/event/AppEventPast'
+export default {
+  components: {
+    AppEventUpcoming,
+    AppEventPast
+  },
+  data() {
+    return {
+      ChapterDetails: ChapterDetails
+    }
+  }
+}
 </script>
-
-<style></style>
